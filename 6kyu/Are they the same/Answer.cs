cee450cd7bcb17a4c1411215a2a22d7fbef344fa
MutanPlex <barisal54@gmail.com>
2022-08-28 03:44:11 +0300

    6 kyu Are they the same Answer Update

--- 6kyu/Are they the same/Answer.cs
+++ 6kyu/Are they the same/Answer.cs
@@ -1,35 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-class AreTheySame
-{
-  public static bool comp(int[] a, int[] b)
-  {
-    if ((a.Length == 0 && b.Length == 0))
-                return true;
-            if (a.Length == 0 || b.Length == 0 || a == null || b == null)
-                return false;
-            if (a.Length != b.Length)
-                return false;
-            int countT = 0;
-            foreach (int i in b)
-            {
-                bool check = false;
-                foreach(int j in a)
-                {
-                    if (Math.Sqrt(i) == j)
-                    {
-                        check = true;
-                        countT++;
-                        break;
-                    }
-                }
 
-                if (check == false)
-                    return false;
-            }
-            return countT == b.Length ? true : false;
-  }
+public class AreTheySame
+{
+    public static bool comp(int[] a, int[] b)
+    {
+        return b != null && a.Select(x => x * x).OrderBy(x => x).SequenceEqual(b.OrderBy(x => x));
+    }
 }
